254597eac5c452dc4f56c7d1fddac2af55380d6f
Justin Roll <justinroll@Justins-MacBook-Air-2.local>
2024-02-10 17:23:00 -0800

    Working blog with inverted relationships.

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
+class CategoriesInline(admin.TabularInline):
+    model = Category.posts.through    
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoriesInline]
+
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Post, PostAdmin)
